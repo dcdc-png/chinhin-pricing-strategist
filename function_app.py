3c332790a0bf512d9379a9afdc79c18913b567ea
Janson <hohohojanson@gmail.com>
2026-03-08 17:54:06 +0800

    azure-functions test

---
+++ function_app.py
@@ -0,0 +1,15 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="hello")
+def hello(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("HTTP trigger function received a request.")
+
+    name = req.params.get("name", "World")
+
+    return func.HttpResponse(
+        f"Hello, {name}!",
+        status_code=200
+    )
