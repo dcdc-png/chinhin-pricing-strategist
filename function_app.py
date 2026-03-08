d08f42e709df31c26b888b8b1043dee5620272cb
Janson <hohohojanson@gmail.com>
2026-03-08 18:12:38 +0800

    CI test

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="hello")
 def hello(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("HTTP trigger function received a request.")
+    logging.info("HTTP trigger function received a request 123456789.")
 
     name = req.params.get("name", "World")
 
