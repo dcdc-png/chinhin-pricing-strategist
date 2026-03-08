f4c48f9eb50edee8a1fe8bd8572a644deabc9e14
Janson <hohohojanson@gmail.com>
2026-03-09 01:07:34 +0800

    function app fix

--- function_app.py
+++ function_app.py
@@ -10,3 +10,7 @@ from blueprints.pricing import bp, load_excel
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 app.register_blueprint(bp)
+
+@app.route(route="ping", methods=["GET"])
+def ping(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("pong", status_code=200)
