9281577897640ce5f692e07c42da6965913d88e5
dc <dcdc-png>
2026-03-08 17:25:08 +0800

    cicd v5

--- frontend/pages/project/[path].js
+++ frontend/pages/project/[path].js
@@ -86,10 +86,13 @@ export async function getStaticProps({ params }) {
   const project = projects.find((proj) => proj.slug === params.path);
   const res = await fetch(`https://api.github.com/repos/${project.path}`);
   const data = await res.json();
-  project.open_issues = data.open_issues;
-  project.subscribers_count = data.subscribers_count;
-  project.stargazers_count = data.stargazers_count;
-  return { props: { project } };
+  
+  const projectProps = { ...project };
+  projectProps.open_issues = data.open_issues ?? 0;
+  projectProps.subscribers_count = data.subscribers_count ?? 0;
+  projectProps.stargazers_count = data.stargazers_count ?? 0;
+
+  return { props: { project: projectProps } };
 }
 
 export default Project;
